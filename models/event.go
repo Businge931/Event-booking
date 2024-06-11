d735127591137ff483f5d750729bf9255d8fbba3
Businge931 <jimmybusinge9@gmail.com>
2024-06-11 15:00:48 +0300

    11. Getting Events From Database (SELECT)

--- models/event.go
+++ models/event.go
@@ -5,7 +5,6 @@ import (
 
 	"example.com/event-booking/db"
 )
-
 type Event struct {
 	ID          int64
 	Name        string    `binding:"required"`
@@ -18,22 +17,20 @@ type Event struct {
 var events = []Event{}
 
 func (e Event) Save() error {
-	query := `INSERT INTO events(name,description,location,dateTime,user_id) 
-	VALUES ("?,?,?,?,?")
-	`
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
 	stmt, err := db.DB.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
-
 	if err != nil {
 		return err
 	}
 	id, err := result.LastInsertId()
 	e.ID = id
-
 	return err
 }
 
@@ -43,14 +40,20 @@ func GetAllEvents() ([]Event, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	defer rows.Close()
 
 	var events []Event
 
 	for rows.Next() {
 		var event Event
-		rows.Scan(&event.ID)
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
 	}
+
 	return events, nil
 }
